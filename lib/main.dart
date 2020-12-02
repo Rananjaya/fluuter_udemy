e3480eb800d7c9b75fb0aa443e272e29f55aa2e9
Rananjaya Bandara <rananjayabandara@gmail.com>
2020-12-02 14:49:19 +0530

    scaffold widget flutter

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() { //Main funcation is strating point of all our appes
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("I'm Rich"),
+        ),
+      ),
+    ),
+  );
+}
