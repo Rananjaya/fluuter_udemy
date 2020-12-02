6e2651728f6ee86890fc4883d9d5ae7d7d8f5c83
Rananjaya Bandara <rananjayabandara@gmail.com>
2020-12-02 15:11:24 +0530

    add background color for Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ void main() { //Main funcation is strating point of all our appes
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           title: Text("I'm Rich"),
           backgroundColor: Colors.blueGrey[900],
         ),
+
       ),
     ),
   );
