88acbe35af29ef0215a61cee3971a5eae85b9a45
Rananjaya Bandara <rananjayabandara@gmail.com>
2020-12-02 15:16:57 +0530

    add body prop and add Center widget and add Text widget for it's child prop

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,11 @@ void main() { //Main funcation is strating point of all our appes
           title: Text("I'm Rich"),
           backgroundColor: Colors.blueGrey[900],
         ),
+        body: Center(
+          child: Text(
+            "Hello World"
+          ),
+        ),
 
       ),
     ),
